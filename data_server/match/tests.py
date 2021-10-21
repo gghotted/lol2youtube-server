472bbe45da8b15e1122af0702a1d98a4663517ef
gghotted <gghotted2@naver.com>
2021-10-22 01:43:40 +0900

    기능: participant 모델 추가, Close #7

--- data_server/match/tests.py
+++ data_server/match/tests.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.test import TestCase
 from raw_data.riot_api import MatchListAPI
 
-from match.models import Match
+from match.models import Match, Participant
 
 
 class MatchCreateTest(TestCase):
@@ -23,3 +23,26 @@ class MatchCreateTest(TestCase):
         match = Match.objects.create_or_get_from_api(match_id=self.match_list[0])
         self.assertTrue(hasattr(match, 'pk'))
         self.assertEqual(match.participants.count(), settings.MATCH_LIST_COUNT)
+
+
+class ParticipantCreateTest(TestCase):
+    match_id = os.environ['TEST_MATCH_ID']
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.match = Match.objects.create_or_get_from_api(match_id=cls.match_id)
+
+    def test_create_participant(self):
+        data = self.match.json.dot_data.info.participants[0]
+        participant = Participant.objects.create(
+            match=self.match,
+            **Participant.parse_json(data)
+        )
+
+        self.assertEqual(participant.champion, data.championName)
+        self.assertEqual(participant.index, data.participantId)
+
+        self.assertEqual(participant.summoner.name, data.summonerName)
+        self.assertEqual(participant.summoner.puuid, data.puuid)
+
+
