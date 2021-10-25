1950ad8ba8d3c4babb7e294f445fa227922e5279
gghotted <gghotted2@naver.com>
2021-10-25 09:50:07 +0900

    기능: kill length값 랭크에 반영, Close #25

--- data_server/event/managers.py
+++ data_server/event/managers.py
@@ -32,7 +32,13 @@ class ChampionKillQuerySet(models.QuerySet):
             nor_damage=self.normalize('avg_damage', min_max),
             nor_avg_damage_contribution=self.normalize('avg_damage_contribution', min_max),
             nor_interval=self.normalize('avg_interval', min_max, reverse=True),
-            interested_score=(F('nor_damage') + F('nor_avg_damage_contribution') + F('nor_interval')),
+            nor_length=self.normalize('length', min_max),
+            interested_score=(
+                F('nor_damage') +
+                F('nor_avg_damage_contribution') +
+                F('nor_interval') +
+                F('nor_length')
+            ),
             rank=Window(DenseRank(), order_by=F('interested_score').desc())
         )
 
@@ -40,13 +46,18 @@ class ChampionKillQuerySet(models.QuerySet):
         return self.aggregate(
             Min('avg_damage'), Max('avg_damage'),
             Min('avg_damage_contribution'), Max('avg_damage_contribution'),
-            Min('avg_interval'), Max('avg_interval')
+            Min('avg_interval'), Max('avg_interval'),
+            Min('length'), Max('length'),
         )
 
     @staticmethod
     def normalize(name, min_max, reverse=False):
         min = Value(min_max[name + '__min'])
         max = Value(min_max[name + '__max'])
+
+        if min == max:
+            return Value(1)
+
         normalized = (F(name) - min) / (max - min)
         return normalized if not reverse else (1 - normalized)
 
