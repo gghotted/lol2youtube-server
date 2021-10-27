d3ed83052915b98667788290b2c7b89add413edd
gghotted <gghotted2@naver.com>
2021-10-27 15:56:45 +0900

    기능: replay 모델 추가, Close #10

--- data_server/replay/models.py
+++ data_server/replay/models.py
@@ -1,3 +1,18 @@
+from common.models import BaseModel
 from django.db import models
 
-# Create your models here.
+
+class Replay(BaseModel):
+    uploaded_at = models.DateTimeField()
+    is_uploaded = models.BooleanField(default=False)
+    file = models.FileField(upload_to='replay/%Y/%m/%d')
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    yt_link = models.URLField()
+    # tags = models.ForeignKey('youtube.Tag', on_delete=models.DO_NOTHING)
+    # yt_account = models.ForeignKey('youtube.Account', on_delete=models.DO_NOTHING)
+
+
+class KillReplay(Replay):
+    kill_event = models.ForeignKey('event.ChampionKill', on_delete=models.DO_NOTHING)
+
