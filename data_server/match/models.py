4be7485bb8bd16c5d081069c2328e6decdbcf51a
gghotted <gghotted2@naver.com>
2021-12-05 03:19:36 +0900

    기능: participant 모델 업데이트 step2

--- data_server/match/models.py
+++ data_server/match/models.py
@@ -73,8 +73,7 @@ class Participant(BaseModel):
     match = models.ForeignKey(Match, models.CASCADE, related_name='participants')
     summoner = models.ForeignKey('summoner.Summoner', models.DO_NOTHING, related_name='participants')
     index = models.PositiveIntegerField()
-    champion = models.CharField(max_length=64)
-    _champion = models.ForeignKey('champion.Champion', models.DO_NOTHING, null=True, related_name='participants')
+    champion = models.ForeignKey('champion.Champion', models.DO_NOTHING, related_name='participants')
 
     def get_champion(self):
         return str(self.champion)
